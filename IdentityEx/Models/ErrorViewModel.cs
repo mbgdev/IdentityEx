9f9380297b0a250859e978d86c44aed41818889d
mbgdev <mb.gurener@gmail.com>
2023-08-07 14:39:30 +0300

    Add project files.

---
+++ IdentityEx/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityEx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
