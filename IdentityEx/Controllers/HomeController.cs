8328d82ab51d033d197ced69e1ef2e3ff524417d
mbgdev <mb.gurener@gmail.com>
2023-08-08 15:07:40 +0300

    SignUp Finish

--- IdentityEx/Controllers/HomeController.cs
+++ IdentityEx/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using IdentityEx.Models;
+using IdentityEx.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace IdentityEx.Controllers
 {
@@ -8,9 +11,11 @@ namespace IdentityEx.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<AppUser> _userManager;
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -22,6 +27,56 @@ namespace IdentityEx.Controllers
         {
             return View();
         }
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = await _userManager.CreateAsync(new() { UserName = model.UserName, PhoneNumber = model.PhoneNumber, Email = model.Email }, model.Password);
+
+            if(result.Succeeded)
+            {
+                TempData["Message"] = "Üyelik işlemi başarılı bir şekilde gerçekleşmiştir.";
+                return View(nameof(HomeController.SignUp));
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+            return View();
+
+      
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ IdentityEx/ViewModels/SignUpVM.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityEx.ViewModels
+{
+    public class SignUpVM
+    {
+      
+        [Required(ErrorMessage ="{0} alanı boş geçilemez.")]
+        [Display(Name = "Kullanıcı Adı :")]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage = "{0} alanı boş geçilemez.")]
+        [EmailAddress(ErrorMessage = "{0} geçerli bir e-posta formatı giriniz.")]
+        [Display(Name = "E-Posta :")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı boş geçilemez.")]
+        [Display(Name = "Telefon :")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı boş geçilemez.")]
+        [Display(Name = "Şifre :")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı boş geçilemez.")]
+        [Display(Name = "Şifre Tekrar :")]
+        [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor.")]
+        public string PasswordConfirm { get; set; }
+
+
+    }
+}
